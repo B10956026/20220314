16edfe188cb6921aef09058d9b8e9e1cce6b5ad9
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:58:56 +0800

    C#1_p.71~p.72 練習

--- 20220314/Program.cs
+++ 20220314/Program.cs
@@ -10,7 +10,33 @@ namespace _20220314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("C#1_p.71~p.72 練習:");
+            Console.WriteLine();
+            //string[] title = new string[] { "座號", "姓名", "電腦概論","程式設計","動畫設計"};
+            string[,] student = new string[,] { { "1", "Jack" },{ "2", "Mary" },{ "3", "Tom" },{ "4", "Grace" },{ "5", "Alice" } };
+            int[,] score = new int[,] { { 80,75,60},{65,67,62},{100,93,91},{98,25,50},{83,82,87}};
+
+            while (true)
+            {
+                Console.Write("輸入座號(1-5)：");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num > 5)
+                {
+                    Console.WriteLine("座號超出範圍");
+                }
+                else
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Console.WriteLine("1. 姓名:" + student[num - 1, 1]);
+                        Console.WriteLine("2. 電腦概論:" + score[num - 1, 0]);
+                        Console.WriteLine("3. 程式設計:" + score[num - 1, 1]);
+                        Console.WriteLine("4. 動畫設計:" + score[num - 1, 2]);
+                    }
+                    break;
+                }
+            }
+         
         }
     }
 }
