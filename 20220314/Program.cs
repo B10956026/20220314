8cda280ca41b25993b7da109d3b6496fb3060fdd
B10956026 <84659977+B10956026@users.noreply.github.com>
2022-03-14 19:41:14 +0800

    C#1_p.71~p.72 練習

--- 20220314/Program.cs
+++ 20220314/Program.cs
@@ -8,58 +8,35 @@ namespace _20220314
 {
     class Program
     {
-        public static void Main (string[] args) {
-            Console.WriteLine("C#1 p.51 練習：");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C#1_p.71~p.72 練習:");
+            Console.WriteLine();
+            //string[] title = new string[] { "座號", "姓名", "電腦概論","程式設計","動畫設計"};
+            string[,] student = new string[,] { { "1", "Jack" },{ "2", "Mary" },{ "3", "Tom" },{ "4", "Grace" },{ "5", "Alice" } };
+            int[,] score = new int[,] { { 80,75,60},{65,67,62},{100,93,91},{98,25,50},{83,82,87}};
 
-            Console.WriteLine();
-    
-            t1(); //呼叫 練習1 function
-    
-            Console.WriteLine();
-    
-            t2(); //呼叫 練習2 function
-    
-            Console.WriteLine();
-    
-            t3(); //呼叫 練習3 function
-
-            Console.ReadKey();
+            while (true)
+            {
+                Console.Write("輸入座號(1-5)：");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num > 5)
+                {
+                    Console.WriteLine("座號超出範圍");
+                }
+                else
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Console.WriteLine("1. 姓名:" + student[num - 1, 1]);
+                        Console.WriteLine("2. 電腦概論:" + score[num - 1, 0]);
+                        Console.WriteLine("3. 程式設計:" + score[num - 1, 1]);
+                        Console.WriteLine("4. 動畫設計:" + score[num - 1, 2]);
+                    }
+                    break;
+                }
+            }
+         
         }
-
-          //練習1.輸入N, 印出 function
-          static void t1(){
-            Console.WriteLine("練習1.輸入N, 印出:");
-            Console.Write("Input num:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("num:"+num);
-            for(int i=0;i<num;i++){
-              for(int j=0;j<=i;j++){
-                Console.Write("*");
-              }
-              Console.WriteLine();
-            }
-          }
-
-          //練習2.輸入N, 印出 function
-            static void t2(){
-            Console.WriteLine("練習2.輸入N, 印出:");
-            Console.Write("Input num:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("num:"+num);
-            for(int i=1;i<=num;i++){
-              Console.WriteLine(num+"*"+i+"="+(num*i));
-            }
-          }
-  
-          //練習3.用兩個迴圈 做出九九乘法表 function
-          static void t3(){
-            Console.Write("練習3.用兩個迴圈 做出九九乘法表：");
-            for(int i=1;i<10;i++){
-              Console.WriteLine();
-              for(int j=1;j<10;j++){
-                Console.WriteLine(i+"*"+j+"="+(i*j));
-              }
-            }  
-          }
     }
 }
